d661becd251a4a3cbd151a9a36c06ec9c5429338
ebubesunday16 <135783723+ebubesunday16@users.noreply.github.com>
2024-09-23 08:16:27 +0100

    Fixing Image Bug

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ export default function About(){
 
     return (
         <>
-            <img src="../images/image54.png"/>
+            <img src="../public/images/image54.png"/>
             <div className="van-life-container">
 
 
